9491841a89a1da3e2c1da77401c1c085994f2abd
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-04-20 21:51:21 -0500

    solve port busy issue and implement logging

--- server.py
+++ server.py
@@ -1,22 +1,37 @@
+from datetime import datetime
 from http.server import SimpleHTTPRequestHandler
-from os import environ
+from logging import basicConfig, getLogger, INFO
+from os import getcwd, environ, listdir, system as executor
 from socketserver import TCPServer
 from threading import Thread
+
+# create logs directory if not found
+executor(f'mkdir logs') if 'logs' not in listdir(getcwd()) else None
+
+# initiate logger
+LOG_FILENAME = datetime.now().strftime('logs/private_cloud_%H:%M:%S_%d-%m-%Y.log')
+basicConfig(filename=LOG_FILENAME, level=INFO, format='%(asctime)s %(levelname)s %(message)s')
+logger = getLogger(__name__)
 
 
 class NetworkManager(SimpleHTTPRequestHandler):
     def do_GET(self) -> None:
-        print(f'Currently Displaying: {self.path}')  # self.path will be the current directory displayed as /
+        logger.info(f'Currently Displaying: {self.path}')  # self.path will be the current directory displayed as /
         SimpleHTTPRequestHandler.do_GET(self)
 
 
 def initiate_host():
     host = '127.0.0.1'
-    print(f'Access it using http://{host}:{port}')
+    logger.info(f'Access it using http://{host}:{port}')
     try:
-        TCPServer((host, port), NetworkManager).serve_forever()
+        handler = TCPServer((host, port), NetworkManager)
+        handler.serve_forever()
     except KeyboardInterrupt:
         pass
+    except OSError as os_error:
+        if str(os_error) == '[Errno 48] Address already in use':
+            # todo: check for 256 output and skip it as that's from the os module
+            print(f"Port {port} is busy. Currently used by the PID {executor(f'lsof -i :{port}')}")
 
 
 if __name__ == '__main__':
