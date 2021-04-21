befd09e22e7cc38bbfa1a5ba78e8b0073fb7deec
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-04-21 10:57:51 -0500

    implement function level logging for visibility and restructure code

--- server.py
+++ server.py
@@ -5,28 +5,19 @@ from os import path, chdir, getcwd, environ, listdir, system as executor
 from socketserver import TCPServer
 from subprocess import check_output, SubprocessError
 from threading import Thread
-
-# create logs directory if not found
-executor(f'mkdir logs') if 'logs' not in listdir(getcwd()) else None
-
-# initiate logger
-LOG_FILENAME = datetime.now().strftime('logs/private_cloud_%H:%M:%S_%d-%m-%Y.log')
-basicConfig(filename=LOG_FILENAME, level=INFO, format='%(asctime)s %(levelname)s %(message)s')
-logger = getLogger(__name__)
-
-# Change this path to the directory on your computer which you want to make accessible by the server.
-# Currently set to the user's home directory
-host_path = path.expanduser('~')
+from socket import gethostbyname
 
 
 class NetworkManager(SimpleHTTPRequestHandler):
     def do_GET(self) -> None:
+        logger = getLogger('do_GET')
         logger.info(f'Currently Accessing: {host_path + self.path}')  # self.path = current directory
         SimpleHTTPRequestHandler.do_GET(self)
 
 
 def initiate_host():
-    host = '127.0.0.1'
+    logger = getLogger('initiate_host')
+    host = gethostbyname('localhost')
     logger.info(f'Access it using http://{host}:{port}')
     try:
         chdir(host_path)
@@ -46,7 +37,14 @@ def initiate_host():
 
 
 if __name__ == '__main__':
-    if (port := int(environ.get('PORT'))) and (user_ := environ.get('USER')) and (pass_ := environ.get('PASS')):
+    if (port := int(environ.get('PORT'))) and (user_ := environ.get('USERNAME')) and (pass_ := environ.get('PASSWORD')):
+        executor(f'mkdir logs') if 'logs' not in listdir(getcwd()) else None  # create logs directory if not found
+        LOG_FILENAME = datetime.now().strftime('logs/private_cloud_%H:%M:%S_%d-%m-%Y.log')  # set log file name
+        basicConfig(filename=LOG_FILENAME, level=INFO, format='%(asctime)s %(levelname)s %(message)s')  # setup config
+
+        host_path = path.expanduser('~')  # path that will be hosted
+        script_path = getcwd()  # current path of the script
+
         Thread(target=initiate_host).start()
     else:
-        print('Please add the environment variables for USER, PASS and desired PORT')
+        print('Please add the environment variables for USERNAME, PASSWORD and desired PORT')
