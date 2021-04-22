e5972ed59aaaa7f993dc9b807dc97636418d22c9
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-04-22 01:55:59 -0500

    add todos for future iteration

---
+++ todos.py
@@ -0,0 +1,12 @@
+"""
+Short Term
+# todo: sync sleep time in python script with the timer in html page
+# todo: have a drop down button as 'Categories' for Movies, Music and Images
+# todo: For images add MFA using AWS SNS to trigger a random generated integer code and validate before granting access
+# todo: hash the hyperlink in html using js and compare it against hashed value of username and password
+# todo: create a cert and bind to the local host server and make ngrok use https forwarding for tcp tunnels
+
+Long Term
+# todo: get rid of static html and using jinja template or similar for easy iterations in future
+# todo: host the webpage using flask or django
+"""
