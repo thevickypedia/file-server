1e48567fc61f7d780a6ab8331ee4c81c7bbf8d95
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-04-22 01:55:06 -0500

    block on failed auth and unblock after timed wait

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from socket import gethostbyname
 from socketserver import TCPServer
 from subprocess import check_output
 from threading import Thread
+from time import sleep
 
 
 class NetworkManager(SimpleHTTPRequestHandler):
@@ -19,18 +20,40 @@ class NetworkManager(SimpleHTTPRequestHandler):
             pass
 
 
+def reset_auth():
+    global login_attempts, thread
+    thread = True
+    sleep(300)
+    login_attempts = 0
+    thread = False
+    return
+
+
 def check_auth(hyperlink):
-    # todo: make auth look for number of attempts and block user if max tries reached
     logger = getLogger('check_auth')
+    global login_attempts
+    if login_attempts >= 2:
+        logger.warning(f'{login_attempts} failed login attempts has been detected.'
+                       f'Login page has been blocked for 5 minutes.')
+        chdir(script_path + '/block')
+        if not thread:
+            Thread(target=reset_auth).start()
+        return
     if "username=" in hyperlink and "password=" in hyperlink:
         result = hyperlink.split('=')
         username = result[1].split('&')[0]
         password = result[-1]
         if username and password and username == environ.get('USERNAME') and password == environ.get('PASSWORD'):
+            if login_attempts:
+                logger.info(f'User {username} successfully logged in after {login_attempts} attempts. '
+                            f'Resetting login attempts to 0.')
+                login_attempts = 0
             logger.info(f'Received username: {username} and password: {password}')
             logger.info(f'Granting Access to {host_path + hyperlink}')
             chdir(host_path)
         elif username and password:
+            login_attempts += 1
+            logger.info(f'Failed login attempt: {login_attempts}')
             logger.info(f'Received username: {username} and password: {password}')
             logger.info(f'No access was granted. Redirected to {script_path}')
             chdir(script_path)
@@ -71,6 +94,8 @@ if __name__ == '__main__':
         LOG_FILENAME = datetime.now().strftime('logs/private_cloud_%H:%M:%S_%d-%m-%Y.log')  # set log file name
         basicConfig(filename=LOG_FILENAME, level=INFO, format='%(asctime)s %(funcName)s %(message)s',
                     datefmt='%b-%d-%Y %H:%M:%S')  # setup config
+
+        login_attempts, thread = 0, False
 
         host_path = path.expanduser('~')  # path that will be hosted
         script_path = getcwd()  # current path of the script
