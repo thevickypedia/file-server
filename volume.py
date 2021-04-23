b3b59306d59fb9a333d1c66e3ef8c1ff0b46eeaf
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-04-23 11:20:57 -0500

    mound and unmount disk programmatically

--- volume.py
+++ volume.py
@@ -1,17 +1,22 @@
 from subprocess import check_output
-import os
-volume_name = input("Enter the Volume Name you'd like to kill usage for:\n")
-pid_check = check_output(f"lsof /Volumes/{volume_name}", shell=True)
+from os import environ, system
+volume_label = input("Enter the name of the Volume you'd like to handle:\n")
+pid_check = check_output(f"echo {environ.get('PASSWORD')} | sudo -S lsof /Volumes/{volume_label} 2>&1;", shell=True)
 pid_list = pid_check.decode('utf-8').split('\n')
 for id_ in pid_list:
-    os.system(f'kill -9 {id_.split()[1]} >/dev/null 2>&1')  # redirects stderr output to stdout
+    system(f'kill -9 {id_.split()[1]} >/dev/null 2>&1') if id_ else None
 
 disk_check = check_output(f"diskutil list 2>&1;", shell=True)
 disk_list = disk_check.decode('utf-8').split('\n\n')
 condition = '(external, physical):'
+mount_uuid = None
 for disk in disk_list:
     if disk and condition in disk:
-        unmount_value = disk.split('\n')[0].strip(condition)
+        unmount_uuid = disk.split('\n')[0].strip(condition)
         disk_info = disk.split('\n')[-1].split()
-        if volume_name in disk_info:
-            os.system(f'diskutil unmountDisk {unmount_value} 2>&1;')
+        if volume_label in disk_info:
+            mount_value = unmount_uuid
+            system(f'diskutil unmountDisk {unmount_uuid} > /dev/null 2>&1;')
+
+system(f'diskutil mountDisk {mount_uuid} > /dev/null 2>&1;')  # mount by disk volume/UUID
+system(f'diskutil mount {volume_label} > /dev/null 2>&1;')  # mount by disk label
