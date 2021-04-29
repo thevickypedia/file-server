07427eab869ffd21ba13edf25ecc3372870abcfb
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-04-29 00:36:59 -0500

    add logger and more end to end socket connection

--- ngrok.py
+++ ngrok.py
@@ -1,17 +1,36 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from logging import getLogger, basicConfig, INFO
+from os import environ
+from pathlib import Path
+from socket import socket, AF_INET, SOCK_STREAM, gethostbyname
 
 from pyngrok.ngrok import connect
 
-port = 1234
-host = '127.0.0.1'
+basicConfig(level=INFO)
+getLogger(name='pyngrok').propagate = False  # disable module level logging
+logger = getLogger(Path(__file__).stem)
+port = environ.get('PORT')
+host = gethostbyname('localhost')
 
-sock = socket(AF_INET, SOCK_STREAM)
+sock = socket(AF_INET, SOCK_STREAM)  # Create a TCP socket
 
-server_address = (host, port)
+# # Uncomment bind to create a whole new connection to the port
+# server_address = (host, port)  # Bind a local socket to the port
+# sock.bind(server_address)  # Bind only accepts tuples
+
 sock.listen(1)
 
-public_url = connect(port, "tcp", options={"remote_addr": f"{host}:{port}"})
-print(public_url)
+public_url = connect(port, "tcp", options={"remote_addr": f"{host}:{port}"})  # Open a ngrok tunnel to the socket
+url = str(public_url).split()[1].replace('"', '').replace('tcp://', 'http://')
 
+connection = None
 while True:
-    sock.accept()
+    try:
+        # Wait for a connection
+        logger.info("Waiting for a connection")
+        connection, client_address = sock.accept()
+        logger.info(f"Connection established from {client_address}")
+    except KeyboardInterrupt:
+        logger.info("Shutting down server")
+        if connection:
+            connection.close()
+        break
