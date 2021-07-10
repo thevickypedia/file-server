866602edef49191502899a306e40de8c779964ae
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-07-09 19:56:43 -0500

    Sunset legacy TCP connection and all code references

--- index.js
+++
@@ -1,7 +0,0 @@
-function success() {
-  if((document.getElementById("username").value==="") || (document.getElementById("password").value==="")) {
-     document.getElementById('auth').disabled = true;
-  } else {
-     document.getElementById('auth').disabled = false;
-  }
-}
