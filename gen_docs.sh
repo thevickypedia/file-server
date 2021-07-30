fcf41ac409eb95dc1f0c5f752701cdde193890d9
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-07-30 00:15:05 -0500

    Auto upgrade `sphinx` before generating docs

--- gen_docs.sh
+++ gen_docs.sh
@@ -4,5 +4,6 @@
 set -e
 rm -rf docs
 mkdir docs
+python3 -m pip install --no-cache --upgrade sphinx
 cp README.md doc_generator && cd doc_generator && make clean html && mv _build/html/* ../docs && rm README.md
 touch ../docs/.nojekyll
